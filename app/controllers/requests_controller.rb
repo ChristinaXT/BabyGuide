dadd65250fb7bb0e8bbf95d9727cefba5f0d0c88
Guess <guess@Guesss-iMac.local>
2019-04-25 13:49:42 -0400

    wrote requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,56 @@
 class RequestsController < ApplicationController
+
+  def index
+    @requests = Request.all
+  end
+
+  def new
+    @request = Request.new(checklist_id: params[:checklist_id])
+  end
+
+  def create
+    @request = Request.create(request_params)
+        if @request.save
+          redirect_to checklist_request_path(@request.checklist, @request)
+      else
+          render 'new'
+       end
+   end
+
+   def show
+     @request = Request.find_by(id: params[:id])
+     @users_request = @request.users_request.find{|user_request|user_request.user_id == current_user.id}
+   end
+
+   def edit
+     @request = Request.find_by(id: params[:id])
+   end
+
+   def update
+     @request = Request.find_by(id: params[:id])
+       if @request.update(task_params)
+         redirect_to checklist_request_path(@request.checklist, @request)
+      else
+         render 'edit'
+       end
+   end
+
+   def destroy
+     request = Request.find_by(id: params[:id])
+     @checklist = request.checklist
+     request.destroy
+     redirect_to checklist_path(@checklist)
+   end
+
+   private
+
+   def request_params
+      params.require(:request).permit(
+        :item,
+        :note, 
+        :checklist_id,
+        :finished,
+        user_ids: []
+      )
+    end
 end
