3cb15886b474ec827268e3a1cc11df548243db26
Guess <guess@Guesss-iMac.local>
2019-04-25 16:36:04 -0400

    updated static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
-  skip_before_action :verify_user_is_authenticated, only: [:homepage]
-  def homepage
+  skip_before_action :verify_user_is_authenticated, only: [:home]
+  def home
   end
 end
