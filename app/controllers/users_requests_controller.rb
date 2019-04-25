3b862aaa160bcf05a47a351481c57a909fb4b620
Guess <guess@Guesss-iMac.local>
2019-04-25 14:19:46 -0400

    users requests controller

---
+++ app/controllers/users_requests_controller.rb
@@ -0,0 +1,17 @@
+class UsersRequestsController < ApplicationController
+
+  def update
+    @user_request = UsersRequest.find_by(id: params[:id])
+      if @user_request.update(users_request_params)
+         redirect_to user_path(@user_request.user_id)
+    else
+         redirect_to user_path(@user_request.user_id)
+      end
+   end
+
+  private
+
+  def users_request_params
+     params.require(:users_request).permit(:closed, :user_id, :request_id)
+  end
+end
