55e30e175c8828391b990f9e4fecd338c66c50d3
Guess <guess@Guesss-iMac.local>
2019-04-25 15:44:10 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,26 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    has_many :
-    has_many :
-    has_many :
+    has_many :checklists
+    has_many :users_request
+    has_many :requests, through: :users_request
 
     def self.find_or_create_by_omniauth(auth_hash)
         where(email: auth_hash["info"]["email"]).first_or_create do |user|
-            user.password = SecureRandom.hex
-            user.name = auth_hash["info"]["name"]
+          user.password = SecureRandom.hex
+          user.name = auth_hash["info"]["name"]
         end
     end
 
-  
+    def open_users_requests
+      UsersRequest.open_by_user(self)
+    end
+
+    def checklist_requests(user_checklist)
+       if checklists.include?(user_checklist)
+         user_checklist.requests
+      else
+           []
+       end
+   end 
 end
