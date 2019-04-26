00ed89b600952d2aeebce6f2f68869f902be005a
Guess <guess@Guesss-iMac.local>
2019-04-26 12:55:50 -0400

    fixed name of welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
-class StaticPageController < ApplicationController
+class WelcomeController < ApplicationController
   skip_before_action :verify_user_is_authenticated, only: [:home]
   def home
   end
