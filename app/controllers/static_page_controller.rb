5e47bd9723fbe73bc73d0c1bde9dc666916ed46f
Guess <guess@Guesss-iMac.local>
2019-04-25 20:57:34 -0400

    started static page

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,4 @@
-class StaticController < ApplicationController
+class StaticPageController < ApplicationController
   skip_before_action :verify_user_is_authenticated, only: [:home]
   def home
   end
